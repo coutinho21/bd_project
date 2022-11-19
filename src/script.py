2cc5e365d36f1d5e3d0e808a1ee2367b2b960998
coutinho21 <guilhermecscoutinho@gmail.com>
2022-11-19 18:51:05 +0000

    Updated povoar.sql

--- src/script.py
+++ src/script.py
@@ -5,48 +5,57 @@ def cleanFile(readFile, writeFile, integers):
     read = open(readFile, 'r')
     write = open(writeFile, 'w')
     lines = read.readlines()
-    
-    for line in lines:
-        count = 0
-        i = 0
-        withoutLinks = ""
-        # remove first 2 links that we dont need
-        while(count < 2):
-            if line[i] == ',':
-                count += 1
-            i += 1
-        withoutLinks = line[i:]
+    aid = 0
 
+    for line in lines:
+        i = 0
+        count = 0
+        outString  = ""
+        aid += 1
+        fields = line.split(',')
+
+        equipaCasa = fields[1]
+        equipaVisitante = fields[2]
+        equipasFile = open("EQUIPA.csv", 'r')
+        equipas = equipasFile.readlines()
+        idEquipaCasa, idEquipaVisitante = 0,0
+
+        for e in equipas:
+            if equipas.index(e) == 0:
+                continue
+
+            fields_equipas = e.split(',')
+            if fields_equipas[1] == equipaVisitante:
+                idEquipaVisitante = fields_equipas[0]
+            elif fields_equipas[1] == equipaCasa:
+                idEquipaCasa = fields_equipas[0]
+
+        outString = fields[0] + ",1," + str(idEquipaCasa) + ',' + str(idEquipaVisitante) + '\n'
         # step ahead first line 
         if lines.index(line) == 0:
-            fields = withoutLinks.split(',')
-            fields = fields[:1] + [fields[len(fields)-1][:len(fields[len(fields)-1])-1]] + fields[1:len(fields)-1]
-            withoutLinks = ','.join(fields)
-            withoutLinks += '\n'
-            write.write(withoutLinks)
+            outString = "idJogo,idEpoca,idEquipaCasa,idEquipaVisitante\n"
+            write.write(outString)
             continue
         
-        count = 0
-        # it is necessary to change the indexes of the fields that are integers 
-        # from each file that is going to be cleaned
-        fields = withoutLinks.split(',')
-        outString = ""
-        for i in range(len(fields)):
-            if i in integers:
-                #remove "" from integer fields
-                fields[i] = fields[i][1:len(fields[i])-1]
+        # count = 0
+        # outString = ""
+        # for i in range(len(fields)):
+        #     if i in integers:
+        #         #remove "" from integer fields
+        #         fields[i] = fields[i][2:len(fields[i])-1]
         
-        # remove last " in line
-        fields[len(fields)-1] = fields[len(fields)-1][:len(fields[len(fields)-1])-1]
-        #put classificacao in right place
-        fields = fields[:1] + [fields[len(fields)-1]] + fields[1:len(fields)-1]
+        # # remove last " in line
+        # fields[len(fields)-1] = fields[len(fields)-1][:len(fields[len(fields)-1])-1]
         # rejoin fields in a string
-        outString = ','.join(fields)
+
         # add a newline
-        outString += '\n'
+        # outString += '\n'
         # write out to the file
         write.write(outString)
 
 
-cleanFile("equipas.csv","EQUIPA.csv",[1,2,3,4,6])
+# it is necessary to change the file names and the 
+# indexes of the fields that are integers 
+# from each file that is going to be cleaned
+cleanFile("JOGOEquipaEpocas.csv","JOGOEQUIPAEPOCA.csv",[4])
 
