7328a9328b1f9dec22f66649e2600d711ef765b7
coutinho21 <guilhermecscoutinho@gmail.com>
2022-12-21 19:19:24 +0000

    Updates on triggers

--- src/triggers/gatilho2_adiciona.sql
+++ src/triggers/gatilho2_adiciona.sql
@@ -7,12 +7,12 @@ PRAGMA foreign_keys = ON;
 -- Verificar se o jogador existe quando é marcado um golo. Se não existir, levanta um erro.
 
 CREATE TRIGGER IF NOT EXISTS existsScorer
-    BEFORE INSERT ON GOLO
+BEFORE INSERT ON GOLO
 BEGIN
-    SELECT
-        CASE
-	    WHEN NOT EXISTS (SELECT * FROM JOGADOR WHERE IDJOGADOR = NEW.IDJOGADOR) THEN
-            RAISE(ABORT, "Jogador não existe.")
-        END;
+SELECT
+    CASE
+    WHEN NOT EXISTS (SELECT * FROM JOGADOR WHERE IDJOGADOR = NEW.IDJOGADOR) THEN
+        RAISE(ABORT, "Jogador não existe.")
+    END;
 END;
 
