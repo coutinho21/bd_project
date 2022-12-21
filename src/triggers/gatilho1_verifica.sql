efcc84d71485f6ba6dd9df84a8baba5273e24ab1
coutinho21 <guilhermecscoutinho@gmail.com>
2022-12-21 18:20:48 +0000

    Added first trigger

---
+++ src/triggers/gatilho1_verifica.sql
@@ -0,0 +1,36 @@
+.mode columns
+.header on
+.nullvalue NULL
+
+PRAGMA foreign_keys = ON;
+
+.print ""
+.print "Initial state:"
+.print ""
+
+SELECT * FROM JOGADOR WHERE IDJOGADOR = 2000;
+
+
+.print ""
+.print "Trying to insert Player with valid number..."
+.print ""
+
+INSERT INTO JOGADOR VALUES (2000, "Jacinto Pinto", "40 anos", 4, "Defesa", 3);
+
+.print ""
+.print "Inserted with sucess:"
+.print ""
+
+SELECT * FROM JOGADOR WHERE IDJOGADOR = 2000;
+
+.print ""
+.print "Trying to insert Player with invalid number..."
+.print ""
+
+INSERT INTO JOGADOR VALUES (2001, "Seixo Paulo", "20 anos", 300, "Avançado", 5);
+
+.print ""
+.print "Not inserted due to trigger"
+.print ""
+
+SELECT * FROM JOGADOR WHERE IDJOGADOR = 2001;
